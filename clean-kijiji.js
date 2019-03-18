372da14503c1a8498b38cf9814efcf1489c938bb
Samay Bhavsar_CONTR <C0601511@vale.com>
2019-03-18 15:01:31 -0400

    Initial commit, removed elements for featured ads

---
+++ clean-kijiji.js
@@ -0,0 +1,10 @@
+var x = document.querySelectorAll(".third-party");
+var i;
+for (i = 0; i < x.length; i++) {
+  x[i].style.display = "none";
+}
+
+x = document.querySelectorAll(".top-feature");
+for (i = 0; i < x.length; i++) {
+  x[i].style.display = "none";
+}
